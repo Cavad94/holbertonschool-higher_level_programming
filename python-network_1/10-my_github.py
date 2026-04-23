4599bc849a9507dce4e99530533956497a23a9c2
Cavad94 <13731@holbertonstudents.com>
2026-04-23 17:18:08 +0000

    Fix pycodestyle W293 in task 10

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -12,12 +12,8 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     url = "https://api.github.com/user"
-
-    # Basic Authentication tələbi üçün auth parametrindən istifadə edirik
     r = requests.get(url, auth=HTTPBasicAuth(username, password))
-    
     try:
-        # JSON cavabını lüğətə çevirib 'id' açarını götürürük
         print(r.json().get('id'))
     except ValueError:
         print("None")
