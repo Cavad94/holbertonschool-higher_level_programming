abc8773a5dd7fb6669884253cfed96b459e65c32
Cavad94 <13731@holbertonstudents.com>
2026-04-23 16:43:53 +0000

    Fix pycodestyle W293: remove whitespace in blank lines

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -12,12 +12,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     values = {'email': email}
-    
-    # Məlumatı URL-encoded formatına salırıq (məs: email=test%40gmail.com)
     data = urllib.parse.urlencode(values)
-    # Məlumatı bayt formatına çeviririk (POST üçün mütləqdir)
     data = data.encode('ascii')
-    
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
