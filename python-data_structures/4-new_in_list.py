5c6fa40f76ea0baff2ba44a53c54b49a0bbea54c
Cavad94 <13731@holbertonstudents.com>
2026-04-30 09:51:58 +0000

    Task 4: Replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Əvvəlcə orijinal siyahının kopyasını yaradırıq
-    copy_list = my_list[:]
-    
-    # İndeksi nüsxə üzərində yoxlayırıq
     if idx < 0 or idx >= len(my_list):
-        return copy_list
-    
-    # Dəyişikliyi yalnız nüsxədə edirik
-    copy_list[idx] = element
-    return copy_list
+        return my_list[:]
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
